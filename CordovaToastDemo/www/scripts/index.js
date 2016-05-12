f6934ff41b13128ae990d4028ef2e9371e27bb08
Martin <Martin.neumayer@stud.fh-rosenheim.de>
2016-05-12 16:56:27 +0200

    Initial Commit

---
+++ CordovaToastDemo/www/scripts/index.js
@@ -0,0 +1,29 @@
+﻿// Eine Einführung zur leeren Vorlage finden Sie in der folgenden Dokumentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// So debuggen Sie Code beim Seitenladen in Ripple oder auf Android-Geräten/-Emulatoren: Starten Sie die App, legen Sie Haltepunkte fest, 
+// und führen Sie dann "window.location.reload()" in der JavaScript-Konsole aus.
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+        var button = document.getElementById("button");
+        button.addEventListener('click', showToast.bind(this), false);
+    };
+
+    function showToast() {
+        window.plugins.toast.show('Your toasts text goes here', 'long', 'bottom');
+    }
+
+    function onPause() {
+        // TODO: Diese Anwendung wurde ausgesetzt. Speichern Sie hier den Anwendungszustand.
+    };
+
+    function onResume() {
+        // TODO: Diese Anwendung wurde erneut aktiviert. Stellen Sie hier den Anwendungszustand wieder her.
+    };
+})();
